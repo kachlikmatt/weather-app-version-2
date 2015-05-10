7097b43df0a94d3d06d6e5d31628e4f9605bbb9c
Victor Bashorun <vbashorun@gmail.com>
2015-05-10 09:07:31 -0500

    implemented JSON retrieval

--- app/src/main/java/edu/noctrl/kachlik/vic/weatherapp2/Main.java
+++ app/src/main/java/edu/noctrl/kachlik/vic/weatherapp2/Main.java
@@ -1,19 +1,48 @@
 package edu.noctrl.kachlik.vic.weatherapp2;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+//url for JSON object: http://craiginsdev.com/zipcodes/findzip.php?zip=60540
+
+// XML url format:
+// http://forecast.weather.gov/MapClick.php?lat={latitude}&lon={longitude}&unit=0&lg=english&FcstType=dwml
+
+// url for XML info:
+// http://forecast.weather.gov/MapClick.php?lat=41.766113999700003&lon=-88.150585000000007&unit=0&lg=english&FcstType=dwml
 
 
 public class Main extends ActionBarActivity {
 
+    LocationIO locationHolder;
+    String jsonUrl;
+
+    public void processJSON()
+    {
+        try
+        {
+            Log.i("processJSON()", locationHolder.locationObject.toString(4));
+        }
+        catch(Exception e){}
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationHolder = new LocationIO();
+        jsonUrl = "http://craiginsdev.com/zipcodes/findzip.php?zip=60540";
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        locationHolder.retrieveJson(jsonUrl, this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
